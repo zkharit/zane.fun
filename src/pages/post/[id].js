6a725073056a899e65039613da88617081a8a2a2
zkharit <zkharit@gmail.com>
2023-12-24 18:26:53 -0500

    Update /post/<id> layout

--- src/pages/post/[id].js
+++ src/pages/post/[id].js
@@ -1,6 +1,8 @@
 import Date from '@/components/date';
+import Layout from '@/components/layout';
 import { getPostData, getPostPaths } from '@/lib/post';
 import Head from 'next/head';
+import utilStyles from '@/styles/utils.module.css';
 
 export async function getStaticPaths() {
   const paths = await getPostPaths();
@@ -23,17 +25,17 @@ export async function getStaticProps({ params }) {
 
 export default function Post({ postData }) {
   return (
-    <>
+    <Layout>
       <Head>
         <title>{ postData.title }</title>
       </Head>
       <article>
-        <h1>{postData.title}</h1>
-          <div>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
       </article>
-    </>
+    </Layout>
   )
 }
