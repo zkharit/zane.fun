7baa024c231622ab1454759414ce1b95c29448de
zkharit <zkharit@gmail.com>
2023-12-27 18:27:13 -0500

    Update layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '@/styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'zane.fun';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'zane.fun';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,13 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="my fun website!"
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
